9c183824833ecdcdf18727e61bc2f4669d344bc6
yashkumar0008 <yashdahiya0008@gmail.com>
2026-04-06 20:52:10 +0530

    update servermvc.js

--- Backend/servermvc.js
+++ Backend/servermvc.js
@@ -30,9 +30,6 @@ app.use((req,res) => {
     res.status(404).send("Invalid URL");
 });
 
-// app.listen(process.env.PORT, () => {
-//     console.log(`Server is running on port ${process.env.PORT}`);
-// });
 
-
-module.exports=app;
+const PORT = process.env.PORT || 2007;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
